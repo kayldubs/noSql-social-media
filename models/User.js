e850c7a309b20d1e45a6a65f034d02c7883a56e4
Kaylee Stevens <kayls@Kaylees-MBP.lan>
2021-12-26 20:57:40 -0600

    all of the models

--- models/User.js
+++ models/User.js
@@ -22,3 +22,11 @@ const userSchema = new Schema ({
     },
     id: false
 });
+
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
